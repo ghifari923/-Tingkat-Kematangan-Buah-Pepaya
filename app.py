8bca516ca0f87a0ed1908fe138abe34cbc606e89
ghifari923 <156482715+ghifari923@users.noreply.github.com>
2026-08-25 22:47:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # app.py dan model_papaya.h5 berada dalam folder yang sama
 MODEL_PATH = os.path.join(
     BASE_DIR,
-    "model.h5"
+    "model_papaya.keras"
 )
 
 IMG_SIZE = (128, 128)
